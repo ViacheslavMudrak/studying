c108fdb33811e06df7505455d2eea280b1d31b73
Viacheslav Mudrak <radost.vend@gmail.com>
2022-02-20 21:40:43 +0300

    Intermediate Book Store added

---
+++ ASP.NET/For students/Iteration 1/BookStore/BookStore/Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly XmlDocument _booksDocument;
+        public BooksController()
+        {
+            _booksDocument = new XmlDocument();
+            _booksDocument.Load("Data/Books.xml");
+        }
+
+        public ViewResult AllBooks()
+        {
+            ViewData["Title"] = "All Books";
+            return View(_booksDocument.DocumentElement.SelectNodes("book"));
+        }
+
+        [Route("Books/AllBooks/{status}")]
+        public ViewResult AllBooks(string status)
+        {
+            string xpath;
+            if (string.Equals(status, "available", StringComparison.InvariantCultureIgnoreCase))
+            {
+                xpath = "book[status='available']";
+                ViewData["Title"] = "Available Books";
+            }
+            else if (string.Equals(status, "programming", StringComparison.InvariantCultureIgnoreCase))
+            {
+                xpath = "book[category='Programming']";
+                ViewData["Title"] = "Books about Programming";
+            }
+            else if (string.Equals(status, "less50", StringComparison.InvariantCultureIgnoreCase))
+            {
+                xpath = "book[price<50]";
+                ViewData["Title"] = "Books cheaper than 50$";
+            }
+            else
+            {
+                xpath = "book";
+                ViewData["Title"] = "All Books";
+            }
+
+            return View(_booksDocument.DocumentElement.SelectNodes(xpath));
+        }
+
+        public ViewResult AllAuthors()
+        {
+            ViewData["Title"] = "All Authors";
+            return View(_booksDocument.DocumentElement.SelectNodes("book"));
+        }
+
+    }
+}
---
+++ ASP.NET/For students/Iteration 1/BookStore/BookStore/Data/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public string Status { get; set; }
+
+    }
+}
